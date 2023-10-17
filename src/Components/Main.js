e8d908d2434b80e9d95640529cb82cf96f164598
Orange Workbook <d.riley0189@gmail.com>
2023-10-17 16:03:21 +0100

    17/10/23 profile update

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -12,26 +12,22 @@ const Main = () => {
                 <div className="skillsDiv">
                     <h3>React</h3>
                     <h3>JavaScript</h3>
-                    <h3>Ruby</h3>
-                    <h3>Python</h3>
                     <h3>Git/Node.js</h3>
                     <h3>CSS/HTML</h3>
-                    
+                    <h3>WordPress</h3>
                 </div>
                 <br></br>
 
                 <h3>Some knowledge of:</h3>
                 <div className="skillsDiv">
-                    <h3>Ruby on Rails</h3>
                     <h3>Php</h3>
+                    <h3>Ruby</h3>
+                    <h3>Ruby on Rails</h3>
                     <h3>JQuery</h3>
                     <h3>TypeScript</h3>
-                    <h3>Java</h3>
-                    <h3>C#</h3>
-                    <h3>WordPress</h3>
+                    <h3>Python</h3>
                     <h3>SQL</h3>
                     <h3>Docker</h3>
-
                 </div>
                 <br></br>
 
@@ -52,14 +48,16 @@ const Main = () => {
 
             <div className="mainSiteChild">
                 <h2 className="mainTitle">Current Goals</h2>
-                <h3 id="goalsSubTitle">My ambition is to become a full-time Web-Developer.</h3>
+                <h3 id="goalsSubTitle">My ambition is to become employed as a full-time Web-Developer.</h3>
                     <p className="mainP">
                         In 2021 I made the decision to career-switch into 
                         programming and I haven't looked back since!
                         
                     </p>
 
-                    <aside>I am currently studying <strong>SQL</strong> and <strong>PHP</strong>.</aside>
+                    <aside>
+                        I am currently doing a <strong>Front End Development Course</strong> through <strong>EDX</strong> to further upskill in React.js and associated technologies.
+                    </aside>
                 
 
                 <br></br>
@@ -70,7 +68,7 @@ const Main = () => {
 
                     After completing a 12-month foundational traineeship in programming last year,
                     I made the top 7% of 20,000 candidates to be accepted onto an intensive Web-Development
-                    course. This was co-certified by the University of Manchester in partnership with the Department for Education 
+                    course specialising in JavaScript/React/CSS. This was co-certified by the University of Manchester in partnership with the Department for Education 
                     specialising in React, JavaScript, Node, Git Bash, HTML and CSS. My final grade was 99%.
                 </p>
 
@@ -79,7 +77,8 @@ const Main = () => {
                 <h2 className="mainTitle">NoteWorthy <span id="titleJobs">Work</span> + <span id="titleEdu">Education</span></h2>
 
                 <ul>
-                <li className="mainULJobs">Web Developer at The Curious, Shrewsbury, UK. 2023.</li>
+                    <li className="mainULEdu">Front-End Development Course. EDX (Remote) 2023 - Current.</li>
+                    <li className="mainULJobs">Web Developer at The Curious, Shrewsbury, UK. 2023.</li>
                     <li className="mainULEdu">Web-Development Bootcamp. University of Manchester/HyperionDev (Remote) 2022 - 2023.</li>
                     <li className="mainULJobs">Junior Ruby on Rails Developer at SuperByte, Shrewsbury, UK. 2022.</li>
                     <li className="mainULEdu">Coding Traineeship. IT Career Switch (Remote) 2021 - 2022.</li>
